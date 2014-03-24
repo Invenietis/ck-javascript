0e3604d93b96c772a98c2afca4b1ef46ccd4de7f
Benjamin Crosnier <benjamin.crosnier@invenietis.com>
2014-03-24 14:34:40 +0100

    CK.Core upgrade (PRERELEASE)

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Invenietis 2013")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyFileVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
 
 #if DEBUG
     [assembly: AssemblyConfiguration("Debug")]
